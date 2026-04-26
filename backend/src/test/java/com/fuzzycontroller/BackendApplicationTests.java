cd2bd934078fec62d27aba7dff8c9f36733b6dd0
Dominik Jałowiecki <dominik.jalowiecki1@gmail.com>
2026-04-26 11:27:48 +0200

    Add backend and frontend

---
+++ backend/src/test/java/com/fuzzycontroller/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuzzycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
